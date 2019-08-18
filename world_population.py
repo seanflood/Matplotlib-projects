d8a12d847a7bfa322e6aae3cb56f0bd49fcedf1f
SEAN FLOOD <42306842+seanflood@users.noreply.github.com>
2019-08-18 16:24:22 -0400

    Add files via upload

---
+++ world_population.py
@@ -0,0 +1,48 @@
+import json 
+
+import pygal 
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
+
+from pygal.maps.world import World
+
+from country_codes import get_country_code 
+
+# Load the data into a list. 
+filename = 'population_data.json'
+with open (filename) as f:
+	pop_data = json.load(f)
+
+# Print the 2010 population for each country. 
+cc_populations = {}
+for pop_dict in pop_data:
+	if pop_dict['Year'] == '2010':
+		country_name = pop_dict['Country Name']
+		population = int(float(pop_dict['Value']))
+		code = get_country_code(country_name)
+		if code:
+			cc_populations[code] = population 
+
+# Group the countries into 3 population levels. 
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+	if pop < 10000000:
+		cc_pops_1[cc] = pop
+	elif pop < 1000000000:
+		cc_pops_2[cc] = pop
+	else:
+		cc_pops_3[cc] = pop 
+
+# See how many countries are in each level. 
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RS('#336699', base_style=LCS)
+wm = World(style=wm_style)
+wm.title = 'World Populations in 2010, by Country'
+wm.add('0-10mil', cc_pops_1)
+wm.add('10mil - 1bil', cc_pops_2)
+wm.add('>1bil', cc_pops_3)
+
+
+wm.render_to_file('world_populations.svg')
+		
+
